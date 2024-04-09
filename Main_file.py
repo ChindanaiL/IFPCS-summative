0be6626d90c53095af65a12cb8be5617428c745d
ChindanaiL <152905913+ChindanaiL@users.noreply.github.com>
2024-04-09 20:17:07 +0700

    Update progres: foundation of slot game done, add comment into code. Waiting for approve from colleagues.

--- Main_file.py
+++ Main_file.py
@@ -1,4 +1,5 @@
 import random
+import time
 print("---Welcome to Slot Machine---")
 print("You will start with £100, you will be asked if you want to play.\nAnswer with yes/y or no/n")
 print("---The rules of the game---")
@@ -14,3 +15,30 @@ firstslot = None #variable for first slot
 secondslot = None #variable for second slot
 thirdslot = None #variable for third slot
 credits = initialcredits
+
+while True: #create loop
+      while credits > 0: #loop while user still have credits
+            print("You have £", credits) #Show user how much credits they have
+            try:
+                  stake_amount = int(input("Please enter stake amount: ")) #Asking for stake amount
+            except: #if user's input is not in integer format
+                  print("Please enter a whole number of amount to stake, Please try again.") #show error message and ask for input again
+
+            if stake_amount>credits: #if user want to stake more than credits they have
+                  print("Not enough credit, please try again.") #show error message
+            else:
+                  credits -= stake_amount #update credits balance after put stake
+                  firstslot = random.choice(itemlist) #random first symbol by using random function
+                  secondslot = random.choice(itemlist) #random second symbol by using random function
+                  thirdslot = random.choice(itemlist) #random third symbol by using random function
+                  print("Stake accepted. Good luck.")
+                  print("Spinning now...")
+                  time.sleep(1) #delay the program 1 second
+                  print()
+                  print("| ", firstslot, " | ", secondslot, " | ", thirdslot, " |\n") #show the result after spin
+
+                  #from here, doing if-else condition to show the condition how to win the game.
+      print("You are out of credits") #if user run out of credit, show message and quit the program.
+      print("Thank you for playing.")
+      time.sleep(1.5)
+      break #exit the loop, end program
